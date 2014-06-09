42d031d695e71229304e4b6f89a693e2b055aa19
ezhil45 <sharmyy@gmail.com>
2014-06-09 16:02:13 +0530

    Initial commit -- wasdev Simple Servlet Sample

---
+++ ServletSample/src/main/java/wasdev/sample/servlet/SimpleServlet.java
@@ -0,0 +1,27 @@
+package wasdev.sample.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SimpleServlet
+ */
+@WebServlet("/*")
+public class SimpleServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().print("<h1><font color=green>Simple Servlet ran successfully</font></h1>"
+                                   + "Powered by WebSphere Application Server Liberty Profile");
+    }
+
+}
