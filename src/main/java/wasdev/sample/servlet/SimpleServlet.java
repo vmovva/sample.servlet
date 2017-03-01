a63511d54fc0fe82101e17c43846fdf523dc60c7
vmovva <vmovva@vamsees-mbp.raleigh.ibm.com>
2017-03-01 16:02:23 -0500

    Added welcome page and changed servlet filter

--- src/main/java/wasdev/sample/servlet/SimpleServlet.java
+++ src/main/java/wasdev/sample/servlet/SimpleServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class SimpleServlet
  */
-@WebServlet("/*")
+@WebServlet("/Simple")
 public class SimpleServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
